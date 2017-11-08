f39cb23b8cb3c1e8b0034ccd3e38a7016170f9de
Martin Arvedahl <martin.arvedahl@gmail.com>
2017-11-08 09:55:20 +0100

    Added size hand2

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -3,8 +3,13 @@
   Read and understood!
 
   3.2:
-
-
+  size hand2
+  = size (Add (Card (Numeric 2) Hearts)(Add (Card Jack Spades) Empty))
+  = 1 + size (Add (Card Jack Spades) Empty)
+  = 1 + 1 + size Empty
+  = 1 + 1 + 0
+  = 2 + 0
+  = 2
 -}
 
 
