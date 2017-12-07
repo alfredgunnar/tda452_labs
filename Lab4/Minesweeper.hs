23b06c8c9c0448d46b486a4eb499c3489d412eaf
Alfred Björk <alfbjo@student.chalmers.se>
2017-12-07 16:26:58 +0100

    Begin writing open function

--- Lab4/Minesweeper.hs
+++ Lab4/Minesweeper.hs
@@ -69,6 +69,17 @@ findMinesOnRows (r:rs) b = findMinesOnRows rs (findMinesOnRow row_num r b)
   where
     row_num = length (rows b) - length (r:rs)
 
+-- | Inserts the given cell at given position in given board
+setCellAt :: Int -> Int -> Cell -> Board -> Board
+setCellAt row col cell b = Board (rows b !!= (row,row''))
+  where
+    row'  = (rows b) !! row
+    row'' = row' !!= (col,cell)
+
+-- | Returns the cell from given position in given board
+getCellAt :: Int -> Int -> Board -> Cell
+getCellAt row col b = ((rows b) !! row) !! col
+
 -- | Given a row number, a row and a complete board this function updates
 -- | the neighbours of every mine in the current row.
 findMinesOnRow :: Int -> [Cell] -> Board -> Board
@@ -95,14 +106,38 @@ incrementAtPositions ((row,col):xs) b =
 -- | this increments the counter in that cell or does nothing if the cell
 -- | is a mine.
 incrementAtPosition :: Int -> Int -> Board -> Board
-incrementAtPosition row col b = Board (rows b !!= (row,row''))
-  where
-    row'  = (rows b) !! row
-    cell  = row' !! col
-    row'' = row' !!= (col,incrementCell cell)
+incrementAtPosition row col b =
+  setCellAt row col (incrementCell (getCellAt row col b)) b
 
 -- | Given a cell, this function increments the counter within.
 -- | If it contains a mine, nothing is done and a mine is returned.
 incrementCell :: Cell -> Cell
 incrementCell (C Mine b) = (C Mine b)
 incrementCell (C (Nearby n) b) = (C (Nearby (n+1)) b)
+
+-----------------------------------------------------------------------------
+-- * Clicking Board
+
+
+-- | Given a row numer, a col number and the board this function returns
+-- | Just Board if everything went okay, otherwise Nothing is returned
+open :: Int -> Int -> Board -> Maybe Board
+open row col b | ct == Mine = Nothing
+               | ct == Nearby 0 = explodeBoardAt row col b
+               | otherwise =
+                 setCellAt row col (clicked (getCellAt row col b)) b
+  where
+    ct = cellTypeAt row col b
+
+cellTypeAt :: Int -> Int -> Board -> CellType
+cellTypeAt row col b = cellType ((rows b !! row) !! col)
+
+cellType :: Cell -> CellType
+cellType (C ct _) = ct
+
+-- | Takes a cell and sets its boolean value to true
+clicked :: Cell -> Cell
+clicked (C ct _) = (C ct True)
+
+explodeBoardAt :: Int -> Int -> Board -> Board
+explodeBoardAt row col b = b
