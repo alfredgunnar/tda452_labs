de3930fc70276496b3b6c42f34aa35a50a9be10a
Alfred Björk <alfbjo@student.chalmers.se>
2017-12-11 16:16:00 +0100

    First playable version in GUI

--- Lab4/GUI_haste.hs
+++ Lab4/GUI_haste.hs
@@ -1,10 +1,10 @@
 import Haste.DOM
 import Haste.Events
-import Test.QuickCheck
 import Minesweeper
+import Data.Maybe (fromJust, isNothing)
 
 data Interface = Interface
-  { iBoard    :: Int -> Int -> Int -> Gen Board
+  { iBoard    :: Int -> Int -> Int -> IO Board
   , iOpen     :: Int -> Int -> Board -> Maybe Board
   }
 
@@ -16,7 +16,7 @@ newBoardElem b = do parent <- newElem "div"
 
 
 newBoardElem' []     = []
-newBoardElem' (r:rs) = children ++ [newElem "br"] ++ (newBoardElem' rs) --children : (newBoardElem' rs)
+newBoardElem' (r:rs) = children ++ [newElem "br"] ++ (newBoardElem' rs)
   where
     children = map newCellElem r
 
@@ -28,66 +28,75 @@ newCellElem c = newElem "input"
 b1 = Board { rows = [[C Mine True,C (Nearby 1) True,C (Nearby 1) True],
                      [C (Nearby 0) True,C Mine False,C Mine False]]}
 
+implementation = Interface
+ { iBoard = rndBoard,
+   iOpen = open
+ }
 
-main = do hello <- newTextElem "Hello"
-          header <- newElem "h1"
-          appendChild header hello
-          appendChild documentBody header
+main = runGame implementation
 
-          gameDiv <- newElem "div"
-          appendChild documentBody gameDiv
+runGame i =
+   do hello <- newTextElem "Hello"
+      header <- newElem "h1"
+      appendChild header hello
+      appendChild documentBody header
 
-          gameBoard <- newBoardElem b1
-          appendChild gameDiv gameBoard
+      b <- (iBoard i 10 10 1)
 
-          rowinput <- newElem "input"
-            `with` [attr "id" =: "row"]
-          rowlabeltxt <- newTextElem "Row"
-          rowlabel <- newElem "label"
-            `with` [attr "for" =: "row"]
-          appendChild rowlabel rowlabeltxt
+      gameDiv <- newElem "div"
+      appendChild documentBody gameDiv
 
-          colinput <- newElem "input"
-            `with` [attr "id" =: "col"]
-          collabeltxt <- newTextElem "Col"
-          collabel <- newElem "label"
-            `with` [attr "for" =: "col"]
-          appendChild collabel collabeltxt
-
-          output <- newElem "input"
-
-          button <- newElem "input"
-                    `with` [attr "type" =: "button",
-                            attr "value" =: "Update"]
-
-          appendChild documentBody rowlabel
-          appendChild documentBody rowinput
-          appendChild documentBody collabel
-          appendChild documentBody colinput
-          appendChild documentBody button
-          appendChild documentBody output
-
-          let reloadBoard r c b = do clearChildren gameDiv
-                                     gameBoard <- newBoardElem b1
-                                     appendChild gameDiv gameBoard
-
-                                     --row <- newTextElem (show r)
-                                     --col <- newTextElem (show c)
-                                     --appendChild gameDiv row
+      gameBoard <- newBoardElem b
+      appendChild gameDiv gameBoard
 
 
-          let update _ = do row <- getProp rowinput "value"
-                            col <- getProp colinput "value"
-                            let r = read row :: Int -- can fail!
-                            let c = read col :: Int -- can fail!
+      rowinput <- newElem "input"
+        `with` [attr "id" =: "row"]
+      rowlabeltxt <- newTextElem "Row"
+      rowlabel <- newElem "label"
+        `with` [attr "for" =: "row"]
+      appendChild rowlabel rowlabeltxt
 
-                            reloadBoard r c gameBoard
+      colinput <- newElem "input"
+        `with` [attr "id" =: "col"]
+      collabeltxt <- newTextElem "Col"
+      collabel <- newElem "label"
+        `with` [attr "for" =: "col"]
+      appendChild collabel collabeltxt
 
-                            setProp output "value" ("(" ++ show r ++ "," ++ show c ++ ")")
-
-          onEvent button Click update
+      output <- newElem "input"
 
 
+      let reloadBoard r c b = do clearChildren gameDiv
+                                 let b' = iOpen i r c b
+                                 if not (isNothing b')
+                                   then do gameBoard <- newBoardElem (fromJust b')
+                                           appendChild gameDiv gameBoard
+                                   else do e <- newTextElem "LOSER"
+                                           appendChild documentBody e
+
+
+      let update _ = do row <- getProp rowinput "value"
+                        col <- getProp colinput "value"
+                        let r = read row :: Int -- can fail!
+                        let c = read col :: Int -- can fail!
+
+                        reloadBoard r c b
+
+                        setProp output "value" ("(" ++ show r ++ "," ++ show c ++ ")")
+
+      button <- newElem "input"
+                  `with` [attr "type" =: "button",
+                          attr "value" =: "Update"]
+
+      appendChild documentBody rowlabel
+      appendChild documentBody rowinput
+      appendChild documentBody collabel
+      appendChild documentBody colinput
+      appendChild documentBody button
+      appendChild documentBody output
+
+      onEvent button Click update
 
 cellToButtonStr :: Cell -> String
 cellToButtonStr (C _          False) = " "
--- Lab4/Minesweeper.hs
+++ Lab4/Minesweeper.hs
@@ -4,20 +4,75 @@ import Test.QuickCheck
 import Util
 import Data.Char hiding (isNumber)
 import Data.Maybe (fromJust, isNothing)
+import System.Random
 
 -- run "cabal install grid" for this to work, then restart ghci
 -- use "haste-cabal install grid" when using haste
 import Math.Geometry.Grid
 import Math.Geometry.Grid.Octagonal
 
-data CellType = Mine | Nearby Int
-  deriving (Eq,Show)
 
+
+-----------------------------------------------------------------------------
+-- * Cell definition and helper functions
 data Cell = C CellType Bool
   deriving (Eq,Show)
 
+data CellType = Mine | Nearby Int
+  deriving (Eq,Show)
+
+cellType :: Cell -> CellType
+cellType (C ct _) = ct
+
+-- | Takes a cell and sets its boolean value to true
+click :: Cell -> Cell
+click (C ct _) = (C ct True)
+
+isClicked :: Cell -> Bool
+isClicked (C _ b) = b
+
+-- | Given a cell, this function increments the counter within.
+-- | If it contains a mine, nothing is done and a mine is returned.
+incrementCell :: Cell -> Cell
+incrementCell (C Mine b) = (C Mine b)
+incrementCell (C (Nearby n) b) = (C (Nearby (n+1)) b)
+
+
+-----------------------------------------------------------------------------
+-- * Board definition and helper functions
 data Board = Board { rows :: [[Cell]] }
   deriving (Eq,Show)
+
+-- | Returns the number of columns in given board
+width :: Board -> Int
+width b = length (rows b !! 0)
+
+-- | Returns the number of rows in given board
+height :: Board -> Int
+height b = length (rows b)
+
+-- | Inserts the given cell at given position in given board
+setCellAt :: Int -> Int -> Cell -> Board -> Board
+setCellAt row col cell b = Board (rows b !!= (row,row''))
+  where
+    row'  = (rows b) !! row
+    row'' = row' !!= (col,cell)
+
+-- | Returns the cell from given position in given board
+getCellAt :: Int -> Int -> Board -> Cell
+getCellAt row col b = ((rows b) !! row) !! col
+
+-- | Given row, col and board this function returns a list of the neighbour
+-- | positions.
+cellNeighbours :: Int -> Int -> Board -> [(Int,Int)]
+cellNeighbours row col b = neighbours grid (row,col)
+  where
+    grid = rectOctGrid (height b) (width b)
+
+-- | Given row, col and board this function returns the type of the cell
+-- | at the given position
+cellTypeAt :: Int -> Int -> Board -> CellType
+cellTypeAt row col b = cellType ((rows b !! row) !! col)
 
 
 -----------------------------------------------------------------------------
@@ -50,14 +105,9 @@ cellToCharClick :: Cell -> Char
 cellToCharClick (C ct clicked) | not clicked = 'X'
                                | otherwise = cellToChar (C ct clicked)
 
+
 -----------------------------------------------------------------------------
 -- * Generating Board
-
--- | Given an int between 0-10 this generates a cell where the int represents
--- | the probability of the cell being a mine.
-cell :: Int -> Gen Cell
-cell n = frequency [ (n, return (C Mine False))
-                   , (10-n, return (C (Nearby 0) False))]
 
 -- | Given a number of columns and a number of rows, this function
 -- | generates a board without any bombs.
@@ -65,13 +115,21 @@ emptyBoard :: Int -> Int -> Board
 emptyBoard rows cols =
   Board [[(C (Nearby 0) False) | _ <- [1..cols]] | _ <-[1..rows]]
 
--- | Given a number of columns, a number of rows and the probability of mines
+-- | Given an int between 0-10 this generates a cell where the int represents
+-- | the probability of the cell being a mine.
+rndCell :: Int -> IO Cell
+rndCell n = do let cells = [(C Mine False) | _ <- [0..n]] ++ [(C (Nearby 0) False) | _ <- [0..(10-n)]]
+               rnd <- randomRIO (0,10)
+               return (cells !! rnd)
+
+-- | Given a number of rows, a number of columns and the probability of mines
 -- | represented by an int between 0-10 this function gives
 -- | a generator for a board.
-board :: Int -> Int -> Int -> Gen Board
-board rows cols prob = do rows <- vectorOf rows (vectorOf cols (cell prob))
-                          let b = setNearbyMarkers (Board rows)
-                          return b
+rndBoard :: Int -> Int -> Int -> IO Board
+rndBoard rows cols prob =
+  do rows <- sequence [sequence [(rndCell prob) | _ <- [1..cols]] | _ <- [1..rows]]
+     let b = setNearbyMarkers (Board rows)
+     return b
 
 -- | Given a board, this function updates the nearby cells of every mine
 -- | with the correct numbers.
@@ -86,17 +144,6 @@ findMinesOnRows (r:rs) b = findMinesOnRows rs (findMinesOnRow row_num r b)
   where
     row_num = length (rows b) - length (r:rs)
 
--- | Inserts the given cell at given position in given board
-setCellAt :: Int -> Int -> Cell -> Board -> Board
-setCellAt row col cell b = Board (rows b !!= (row,row''))
-  where
-    row'  = (rows b) !! row
-    row'' = row' !!= (col,cell)
-
--- | Returns the cell from given position in given board
-getCellAt :: Int -> Int -> Board -> Cell
-getCellAt row col b = ((rows b) !! row) !! col
-
 -- | Given a row number, a row and a complete board this function updates
 -- | the neighbours of every mine in the current row.
 findMinesOnRow :: Int -> [Cell] -> Board -> Board
@@ -106,16 +153,9 @@ findMinesOnRow row_num ((C Mine click):cs) b =
       incrementAtPositions (cellNeighbours row_num col_num b) b
     )
   where
-    col_num = length (rows b !! 1) - length ((C Mine click):cs)
+    col_num = (width b) - length ((C Mine click):cs)
 
 findMinesOnRow row_num (c:cs) b    = findMinesOnRow row_num cs b
-
--- | Given row, col and board this function returns a list of the neighbour
--- | positions.
-cellNeighbours :: Int -> Int -> Board -> [(Int,Int)]
-cellNeighbours row col b = neighbours grid (row,col)
-  where
-    grid = rectOctGrid (length (rows b)) (length (rows b !! 1))
 
 -- | Given a list of positions (row,col) and a board this function increments
 -- | the cells at the positions if they are not mines.
@@ -131,15 +171,9 @@ incrementAtPosition :: Int -> Int -> Board -> Board
 incrementAtPosition row col b =
   setCellAt row col (incrementCell (getCellAt row col b)) b
 
--- | Given a cell, this function increments the counter within.
--- | If it contains a mine, nothing is done and a mine is returned.
-incrementCell :: Cell -> Cell
-incrementCell (C Mine b) = (C Mine b)
-incrementCell (C (Nearby n) b) = (C (Nearby (n+1)) b)
 
 -----------------------------------------------------------------------------
 -- * Clicking Board
-
 
 -- | Given a row numer, a col number and the board this function returns
 -- | Just Board if everything went okay, otherwise Nothing is returned
@@ -151,20 +185,6 @@ open row col b | ct == Mine = Nothing
   where
     ct = cellTypeAt row col b
 
-cellTypeAt :: Int -> Int -> Board -> CellType
-cellTypeAt row col b = cellType ((rows b !! row) !! col)
-
-cellType :: Cell -> CellType
-cellType (C ct _) = ct
-
--- | Takes a cell and sets its boolean value to true
-click :: Cell -> Cell
-click (C ct _) = (C ct True)
-
-isClicked :: Cell -> Bool
-isClicked (C _ b) = b
-
-
 explodeBoardAt :: Int -> Int -> Board -> Board
 explodeBoardAt row col b
   | cellTypeAt row col b == Mine = b
@@ -175,6 +195,7 @@ explodeBoardAt row col b
   where
     nb = cellNeighbours row col b
 
+-- | Helper function for explodeBoardAt
 explodeBoardAtPositions :: [(Int,Int)] -> Board -> Board
 explodeBoardAtPositions [] b = b
 explodeBoardAtPositions ((row,col):ps) b =
